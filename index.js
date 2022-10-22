a31eb10f28525555ed24a9cbdf4673809f8d6c41
abbasraza17 <s.abbasraza17@gmail.com>
2022-10-22 12:42:36 +0500

    interview question solved

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log(`Hello World`);
+
+const arr = [14, 18, 12, 13, 29, 27, 22, 24, 30, 32, 26];
+const target = 24;
+const range = 4;
+const newArr = arr.filter((num) => {
+  return num >= target - range && num <= target + range;
+});
+
+console.log(newArr);
